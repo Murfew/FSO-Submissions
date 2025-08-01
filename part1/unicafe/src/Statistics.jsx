0e78c9bc0094cb03521c553b4545ae81e4c96d55
Murfew <matthew.hurley953@gmail.com>
2025-08-01 16:02:31 -0400

    Completed 1.7

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,9 +1,24 @@
 const Statistics = ({good, neutral, bad}) => {
+  const getAverage = () => {
+    return (good - bad) / getTotal()
+  }
+
+  const getPositive = () => {
+    return good / getTotal() * 100
+  }
+
+  const getTotal = () => {
+    return good + bad + neutral
+  }
+
   return (
     <div>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All : {getTotal()}</p>
+      <p>Average: {getAverage()}</p>
+      <p>Positive: {getPositive()} %</p>
     </div>
   )
 }
