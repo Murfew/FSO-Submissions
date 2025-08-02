f09ce76e56d138d400190bb2df216070b78f2f21
Murfew <matthew.hurley953@gmail.com>
2025-08-02 14:08:48 -0400

    Completed 2.9

--- part2/phonebook/src/components/App.jsx
+++ part2/phonebook/src/components/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const checkDuplicate = (name) => {
     return persons.filter((person) => person.name.toLowerCase() === name.toLowerCase()).length !== 0
@@ -27,6 +31,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Show names containing: <input value={newFilter} onChange={(event) => setNewFilter(event.target.value)}/>
+      </div>
+      <h2>Add new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
@@ -39,7 +47,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {
+        persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase())).map(person => <p key={person.name}>{person.name} {person.number}</p>)
+      }
     </div>
   )
 }
