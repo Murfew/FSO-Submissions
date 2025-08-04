59fdd5b0adfaea10568553e65252e54c78da8142
Murfew <matthew.hurley953@gmail.com>
2025-08-04 12:02:22 -0400

    Completed 2.12

--- part2/phonebook/src/components/App.jsx
+++ part2/phonebook/src/components/App.jsx
@@ -29,7 +29,12 @@ const App = () => {
     if (duplicate) {
       alert(`${newName} is already in the phonebook`)
     } else {
-      setPersons(persons.concat({name: newName, number: newNumber}))
+      const personObject = {name: newName, number: newNumber};
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+        })
     }
     setNewName('')
     setNewNumber('')
--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,7 +1,11 @@
 const Persons = ({persons, filter}) => {
   return (
     <div>
-      {persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase())).map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {
+        persons
+          .filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
+          .map(person => <p key={person.id}>{person.name} {person.number}</p>)
+      }
     </div>
   )
 }
