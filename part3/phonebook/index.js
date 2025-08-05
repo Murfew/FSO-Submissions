74b31a6953aea9c43010d784cbafd160015fe5aa
Murfew <matthew.hurley953@gmail.com>
2025-08-05 15:57:35 -0400

    Completed 3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let data = [
   {
     id: '1',
@@ -52,6 +54,24 @@ app.delete('/api/persons/:id', (request, response) => {
   data = data.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'content missing' });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.random() * 100000000,
+  };
+
+  data = data.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
