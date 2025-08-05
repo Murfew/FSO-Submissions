bf7250dce17370a3ff14fa9b4522e77266875508
Murfew <matthew.hurley953@gmail.com>
2025-08-05 14:21:42 -0400

    Completed 2.19

--- part2/countries/src/components/App.jsx
+++ part2/countries/src/components/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
       {countries.length >= 10 ? (
         <p>Too many matches, specify another filter</p>
       ) : countries.length > 1 ? (
-        <CountryList countries={countries} />
+        <CountryList countries={countries} showCountry={(country) => {setCountries([country])}}/>
       ) : countries.length === 1 ? (
         <Country country={countries[0]} />
       ) : null
--- part2/countries/src/components/CountryList.jsx
+++ part2/countries/src/components/CountryList.jsx
@@ -1,7 +1,7 @@
-const CountryList = ({countries}) => {
+const CountryList = ({countries, showCountry}) => {
   return (
     <>
-   {countries.map((country) => <p key={country.name.common}>{country.name.common}</p>)}
+   {countries.map((country) => <p key={country.name.common}>{country.name.common} <button onClick={() => showCountry(country)}>Show</button></p>)}
     </>
   )
 }
