3df40af2c111073e8d369fe4aed5c60b676d8fe8
Murfew <matthew.hurley953@gmail.com>
2025-08-06 15:33:43 -0400

    Completed 3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,9 +5,8 @@ const Person = require('./models/person');
 
 const app = express();
 
-app.use(express.json());
 app.use(express.static('dist'));
-
+app.use(express.json());
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
@@ -33,10 +32,11 @@ app.get('/api/persons/:id', (request, response) => {
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  data = data.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -55,6 +55,24 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson);
   });
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
