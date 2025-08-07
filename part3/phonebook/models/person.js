042c31dbc8ae92466db2ebc68c659e0f7c5344c9
Murfew <matthew.hurley953@gmail.com>
2025-08-06 20:37:03 -0400

    Completed 3.20

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -22,6 +22,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: (value) => {
+        return /^\d{2,3}-\d+$/.test(value);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
   },
 });
 
