d42647d656fb7288c20d6c29f50acd414768a30c
Murfew <matthew.hurley953@gmail.com>
2025-08-08 11:50:53 -0400

    Completed 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,4 +2,16 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((acc, cur) => acc + cur.likes, 0);
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  return blogs.reduce((acc, cur) => {
+    if (cur.likes > acc.likes) {
+      return cur;
+    }
+    return acc;
+  });
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
