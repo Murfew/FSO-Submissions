51772c5f69eab997c6c4f54135ea5c7ff4a29773
Murfew <matthew.hurley953@gmail.com>
2025-08-09 11:05:46 -0400

    Completed 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -36,6 +36,27 @@ test('all blog posts have id property and no _id', async () => {
   });
 });
 
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'Test Blog',
+    author: 'Tester',
+    url: 'http://example.com',
+    likes: 5,
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+
+  const titles = blogsAtEnd.map((n) => n.title);
+  assert(titles.includes('Test Blog'));
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
