7d873f3bc43b101a0b8e8ef1ad4c8d39e3631ca4
Murfew <matthew.hurley953@gmail.com>
2025-08-21 10:17:20 -0400

    Completed 4.11

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -57,6 +57,18 @@ test('a valid blog can be added ', async () => {
   assert(titles.includes('Test Blog'));
 });
 
+test('if likes is missing, it defaults to 0', async () => {
+  const newBlog = {
+    title: 'Blog without likes',
+    author: 'John Doe',
+    url: 'http://test.com',
+  };
+
+  const response = await api.post('/api/blogs').send(newBlog);
+
+  assert.strictEqual(response.body.likes, 0);
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
