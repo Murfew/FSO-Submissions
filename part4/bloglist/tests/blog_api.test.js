7a6d33059aab85e4a6e077d83e479f222457b4e1
Murfew <matthew.hurley953@gmail.com>
2025-08-21 10:55:19 -0400

    Completed 4.13

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test');
+const { test, after, beforeEach, describe } = require('node:test');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const assert = require('node:assert');
@@ -8,83 +8,103 @@ const helper = require('./test_helper');
 
 const api = supertest(app);
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
-  await Blog.insertMany(helper.initialBlogs);
-});
-
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs');
-
-  assert.strictEqual(response.body.length, helper.initialBlogs.length);
-});
-
-test('blogs are returned as JSON', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
-
-test('all blog posts have id property and no _id', async () => {
-  const response = await api.get('/api/blogs');
-
-  response.body.forEach((blog) => {
-    assert.ok(blog.id);
-    // eslint-disable-next-line no-underscore-dangle
-    assert.strictEqual(blog._id, undefined);
+describe('when there are initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    await Blog.insertMany(helper.initialBlogs);
   });
-});
 
-test('a valid blog can be added ', async () => {
-  const newBlog = {
-    title: 'Test Blog',
-    author: 'Tester',
-    url: 'http://example.com',
-    likes: 5,
-  };
+  test('blogs are returned as JSON', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
 
-  const blogsAtEnd = await helper.blogsInDb();
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+    assert.strictEqual(response.body.length, helper.initialBlogs.length);
+  });
 
-  const titles = blogsAtEnd.map((n) => n.title);
-  assert(titles.includes('Test Blog'));
-});
+  test('all blog posts have id property and no _id', async () => {
+    const response = await api.get('/api/blogs');
 
-test('if likes is missing, it defaults to 0', async () => {
-  const newBlog = {
-    title: 'Blog without likes',
-    author: 'John Doe',
-    url: 'http://test.com',
-  };
+    response.body.forEach((blog) => {
+      assert.ok(blog.id);
+      // eslint-disable-next-line no-underscore-dangle
+      assert.strictEqual(blog._id, undefined);
+    });
+  });
 
-  const response = await api.post('/api/blogs').send(newBlog);
+  describe('adding a new blog', () => {
+    test('succeeds with valid data', async () => {
+      const newBlog = {
+        title: 'Test Blog',
+        author: 'Tester',
+        url: 'http://example.com',
+        likes: 5,
+      };
 
-  assert.strictEqual(response.body.likes, 0);
-});
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-test('if title is missing, respond with 400 Bad Request', async () => {
-  const newBlog = {
-    author: 'John Doe',
-    url: 'http://test.com',
-  };
+      const blogsAtEnd = await helper.blogsInDb();
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
 
-  await api.post('/api/blogs').send(newBlog).expect(400);
-});
+      const titles = blogsAtEnd.map((n) => n.title);
+      assert(titles.includes('Test Blog'));
+    });
 
-test('if url is missing, respond with 400 Bad Request', async () => {
-  const newBlog = {
-    author: 'John Doe',
-    title: 'No URL blog',
-  };
+    test('fails with status code 400 if title is invalid', async () => {
+      const newBlog = {
+        author: 'John Doe',
+        url: 'http://test.com',
+      };
 
-  await api.post('/api/blogs').send(newBlog).expect(400);
+      await api.post('/api/blogs').send(newBlog).expect(400);
+    });
+
+    test('fails with status code 400 if url is invalid', async () => {
+      const newBlog = {
+        author: 'John Doe',
+        title: 'No URL blog',
+      };
+
+      await api.post('/api/blogs').send(newBlog).expect(400);
+    });
+
+    test('sets likes to 0 if it is missing', async () => {
+      const newBlog = {
+        title: 'Blog without likes',
+        author: 'John Doe',
+        url: 'http://test.com',
+      };
+
+      const response = await api.post('/api/blogs').send(newBlog);
+
+      assert.strictEqual(response.body.likes, 0);
+    });
+  });
+
+  describe('deleting a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb();
+      const blogToDelete = blogsAtStart[0];
+
+      await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+      const blogsAtEnd = await helper.blogsInDb();
+
+      const ids = blogsAtEnd.map((blog) => blog.id);
+      assert(!ids.includes(blogToDelete.id));
+
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+    });
+  });
 });
 
 after(async () => {
