ce3ce40c69899dde85035992b2c03377abaf21b7
Murfew <matthew.hurley953@gmail.com>
2025-08-22 11:56:48 -0400

    Completed 4.15

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -5,17 +5,18 @@ const blogSchema = mongoose.Schema({
   author: String,
   url: { type: String, required: true },
   likes: { type: Number, default: 0 },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => ({
-    // eslint-disable-next-line no-underscore-dangle
-    id: returnedObject._id.toString(),
-    title: returnedObject.title,
-    author: returnedObject.author,
-    url: returnedObject.url,
-    likes: returnedObject.likes,
-  }),
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
