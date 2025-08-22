317b8960b750919d1e9ca970b22e79fc5899b052
Murfew <matthew.hurley953@gmail.com>
2025-08-22 12:12:08 -0400

    Completed 4.16

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -5,6 +5,8 @@ const assert = require('node:assert');
 const app = require('../app');
 const Blog = require('../models/blog');
 const helper = require('./test_helper');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
 const api = supertest(app);
 
@@ -128,6 +130,60 @@ describe('when there are initially some blogs saved', () => {
 
       assert.strictEqual(blogsAtEnd[0].likes, 10);
     });
+  });
+});
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('sekret', 10);
+    const user = new User({ username: 'root', passwordHash });
+
+    await user.save();
+  });
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    assert(usernames.includes(newUser.username));
+  });
+
+  test('creation fails with status code 400 and returns proper message if username is already taken', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert(result.body.error.includes('expected `username` to be unique'));
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length);
   });
 });
 
