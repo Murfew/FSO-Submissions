c02f4b26d8b82557eb01506024ef120866243b45
Murfew <matthew.hurley953@gmail.com>
2025-08-22 20:57:31 -0400

    Completed 5.14

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title and author, but not likes or url by default', () => {
@@ -29,4 +30,36 @@ test('renders title and author, but not likes or url by default', () => {
 
   const likes = screen.queryByText('10 likes')
   expect(likes).toBeNull()
+})
+
+// eslint-disable-next-line quotes
+test("renders likes and url once the 'show' button is clicked", async () => {
+  const blog = {
+    title: 'Test blog',
+    author: 'Tester',
+    url: 'example.com',
+    likes: 10,
+    user: { username: 'testuser' },
+  }
+
+  const testUser = { username: 'testuser' }
+
+  render(
+    <Blog
+      blog={blog}
+      handleLike={() => {}}
+      user={testUser}
+      handleRemove={() => {}}
+    />
+  )
+
+  const user = userEvent.setup()
+  const button = await screen.findByText('Show')
+  await user.click(button)
+
+  const url = await screen.findByText('example.com')
+  expect(url).toBeDefined()
+
+  const likes = await screen.findByText('10 likes')
+  expect(likes).toBeDefined()
 })
