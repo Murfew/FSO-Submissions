f8eaa51c989949dbbd9c1970f24d8f43894be14a
Murfew <matthew.hurley953@gmail.com>
2025-08-25 12:28:59 -0400

    Completed 6.8

---
+++ part6/anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+import { useSelector, useDispatch } from 'react-redux'
+import { voteFor } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(voteFor(id))
+  }
+
+  return (
+    <>
+      {anecdotes
+        .sort((a, b) => {
+          return b.votes - a.votes
+        })
+        .map((anecdote) => (
+          <Anecdote
+            key={anecdote.id}
+            anecdote={anecdote}
+            handleClick={() => vote(anecdote.id)}
+          />
+        ))}
+    </>
+  )
+}
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div key={anecdote.id}>
+      <div>{anecdote.content}</div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+export default AnecdoteList
