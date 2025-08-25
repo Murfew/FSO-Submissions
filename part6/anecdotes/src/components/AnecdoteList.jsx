49fd1c9a00717f410cfbd30ff189892727cbb4fe
Murfew <matthew.hurley953@gmail.com>
2025-08-25 13:03:26 -0400

    Completed 6.9

--- part6/anecdotes/src/components/AnecdoteList.jsx
+++ part6/anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteFor } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state)
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    return anecdotes.filter((a) =>
+      a.content.toLowerCase().includes(filter.toLowerCase())
+    )
+  })
   const dispatch = useDispatch()
 
   const vote = (id) => {
