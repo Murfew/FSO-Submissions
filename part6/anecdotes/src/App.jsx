fa0c6bc11411a8f7e4b5fe46666d0812e34ab921
Murfew <matthew.hurley953@gmail.com>
2025-08-26 16:11:30 -0400

    Completed 6.14

--- part6/anecdotes/src/App.jsx
+++ part6/anecdotes/src/App.jsx
@@ -2,8 +2,24 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const fetchAnecdotes = async () => {
+      const anecdotes = await anecdoteService.getAll()
+      dispatch(setAnecdotes(anecdotes))
+    }
+
+    fetchAnecdotes()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div>
       <h2>Anecdotes</h2>
