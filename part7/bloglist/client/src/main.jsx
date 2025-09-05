c134a1eafde1042c3b5f66c316985859db55a9c2
Murfew <matthew.hurley953@gmail.com>
2025-09-05 13:05:24 -0400

    Completed 8.1

--- part7/bloglist/client/src/main.jsx
+++ part7/bloglist/client/src/main.jsx
@@ -1,12 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { UserContextProvider } from './contexts/UserContext'
 import { BrowserRouter as Router } from 'react-router-dom'
-
-import { CssBaseline } from '@mui/material'
 
 import App from './App'
 import { NotificationContextProvider } from './contexts/NotificationContext'
+import { UserContextProvider } from './contexts/UserContext'
 
 const queryClient = new QueryClient()
 
@@ -15,7 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <UserContextProvider>
         <Router>
-          <CssBaseline />
           <App />
         </Router>
       </UserContextProvider>
