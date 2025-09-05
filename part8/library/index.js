e448d63d22203cde26f9e8435edfb10e249d4866
Murfew <matthew.hurley953@gmail.com>
2025-09-05 13:29:14 -0400

    Completed 8.6

--- part8/library/index.js
+++ part8/library/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -111,6 +112,10 @@ const typeDefs = `
     allBooks(author: String, genre:String): [Book!]!,
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(title:String!, author:String!, published:Int!, genres:[String!]!):Book
+  }
 `
 
 const resolvers = {
@@ -130,6 +135,20 @@ const resolvers = {
   Author: {
     bookCount: (root) =>
       books.filter((book) => book.author === root.name).length,
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+
+      if (!authors.find((author) => author.author === args.author)) {
+        const author = { name: args.author, id: uuid() }
+        authors = authors.concat(author)
+      }
+
+      books = books.concat(book)
+      return book
+    },
   },
 }
 
