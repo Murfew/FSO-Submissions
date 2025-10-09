43811c8730e210d993d1e23519f5808ffc5e2b71
Matthew Hurley <matthew.hurley@goto.com>
2025-10-09 16:00:51 -0400

    Completed 8.12

--- part8/library/client/src/components/Authors.jsx
+++ part8/library/client/src/components/Authors.jsx
@@ -49,10 +49,13 @@ const Authors = () => {
       <form onSubmit={submit}>
         <div>
           name
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
+          <select value={name} onChange={({ target }) => setName(target.value)}>
+            {authors.map((author) => (
+              <option key={author.id} value={author.name}>
+                {author.name}
+              </option>
+            ))}
+          </select>
         </div>
         <div>
           born
