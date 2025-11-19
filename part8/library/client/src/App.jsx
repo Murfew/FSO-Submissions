2d5a8bdc7ac0b6148d87b239d0082dd34e940475
Matthew Hurley <matthew.hurley@goto.com>
2025-11-19 15:19:22 -0500

    Completed 8.18

--- part8/library/client/src/App.jsx
+++ part8/library/client/src/App.jsx
@@ -2,8 +2,20 @@ import { Routes, Route, Link } from 'react-router-dom'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
+import { useState } from 'react'
+import { useApolloClient } from '@apollo/client/react'
 
 const App = () => {
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
   return (
     <div>
       <div>
@@ -13,15 +25,24 @@ const App = () => {
         <Link to='/books'>
           <button>books</button>
         </Link>
-        <Link to='/newBook'>
-          <button>add book</button>
-        </Link>
+        {token ? (
+          <Link to='/newBook'>
+            <button>add book</button>
+          </Link>
+        ) : null}
+        {!token ? (
+          <Link to='/login'>
+            <button>login</button>
+          </Link>
+        ) : null}
+        {token ? <button onClick={logout}>logout</button> : null}
       </div>
 
       <Routes>
         <Route path='/' element={<Authors />} />
         <Route path='/books' element={<Books />} />
         <Route path='/newBook' element={<NewBook />} />
+        <Route path='/login' element={<LoginForm setToken={setToken} />} />
       </Routes>
     </div>
   )
