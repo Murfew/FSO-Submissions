263c96f08eceb5e8eb1e6cb792d80a740ba82b63
Murfew <matthew.hurley953@gmail.com>
2026-01-16 11:26:40 -0500

    Completed 9.6

--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -1,4 +1,4 @@
-import { isNotNumber } from './utils'
+import { isNotNumber } from './utils';
 
 interface Result {
   periodLength: number
@@ -16,26 +16,26 @@ interface ExerciseValues {
 }
 
 const parseArguments = (args: string[]): ExerciseValues => {
-  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length < 4) throw new Error('Not enough arguments');
 
   args.slice(2).forEach((arg) => {
-    if (isNotNumber(arg)) throw new Error('Provided values were not numbers')
-  })
+    if (isNotNumber(arg)) throw new Error('Provided values were not numbers');
+  });
 
   return {
     exerciseData: args.slice(3).map((arg) => Number(arg)),
     target: Number(args[2]),
-  }
-}
+  };
+};
 
 const analyze = (exerciseData: number[], target: number): Result => {
-  const periodLength = exerciseData.length
-  const trainingDays = exerciseData.filter((day) => day !== 0).length
-  const average = exerciseData.reduce((acc, cur) => acc + cur) / periodLength
-  const success = average >= target
-  const rating = average < target - 0.5 * target ? 1 : !success ? 2 : 3
+  const periodLength = exerciseData.length;
+  const trainingDays = exerciseData.filter((day) => day !== 0).length;
+  const average = exerciseData.reduce((acc, cur) => acc + cur) / periodLength;
+  const success = average >= target;
+  const rating = average < target - 0.5 * target ? 1 : !success ? 2 : 3;
   const ratingDescription =
-    rating === 3 ? 'good' : rating === 2 ? 'okay' : 'bad'
+    rating === 3 ? 'good' : rating === 2 ? 'okay' : 'bad';
 
   return {
     periodLength,
@@ -45,16 +45,16 @@ const analyze = (exerciseData: number[], target: number): Result => {
     ratingDescription,
     target,
     average,
-  }
-}
+  };
+};
 
 try {
-  const { exerciseData, target } = parseArguments(process.argv)
-  console.log(analyze(exerciseData, target))
+  const { exerciseData, target } = parseArguments(process.argv);
+  console.log(analyze(exerciseData, target));
 } catch (error: unknown) {
-  let errorMessage = 'Something bad happened.'
+  let errorMessage = 'Something bad happened.';
   if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message
+    errorMessage += ' Error: ' + error.message;
   }
-  console.log(errorMessage)
+  console.log(errorMessage);
 }
--- part9/index.ts
+++ part9/index.ts
@@ -1,25 +1,36 @@
-import express from 'express'
-import { isNotNumber } from './utils'
-import { calculateBmi } from './bmiCalculator'
-const app = express()
+import express from 'express';
+import { isNotNumber } from './utils';
+import { calculateBmi } from './bmiCalculator';
+const app = express();
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!')
-})
+  res.send('Hello Full Stack!');
+});
 
 app.get('/bmi', (req, res) => {
-  const height = Number(req.query.height)
-  const weight = Number(req.query.weight)
+  const { height, weight } = req.query;
 
-  if (isNotNumber(height) || isNotNumber(weight)) {
-    res.status(400).json({ error: 'height and weight must be numbers' })
+  if (!height || isNotNumber(height)) {
+    res
+      .status(400)
+      .send({ error: 'Height must be provided and must be a number' });
   }
 
-  res.send({ weight, height, bmi: calculateBmi(height, weight) })
-})
+  if (!weight || isNotNumber(weight)) {
+    res
+      .status(400)
+      .send({ error: 'Weight must be provided and must be a number' });
+  }
 
-const PORT = 3003
+  res.send({
+    weight,
+    height,
+    bmi: calculateBmi(Number(height), Number(weight)),
+  });
+});
+
+const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
