70f7a910d504ac3779fd49c6207c86877012bff6
Murfew <matthew.hurley953@gmail.com>
2026-01-23 15:33:14 -0500

    Fix enum issues

--- part9/patientor/server/src/utils.ts
+++ part9/patientor/server/src/utils.ts
@@ -5,7 +5,7 @@ export const NewPatientSchema = z.object({
   name: z.string(),
   dateOfBirth: z.string().pipe(z.iso.date()),
   ssn: z.string(),
-  gender: z.enum(Gender),
+  gender: z.enum([Gender.Male, Gender.Female, Gender.Other]),
   occupation: z.string()
 });
 
