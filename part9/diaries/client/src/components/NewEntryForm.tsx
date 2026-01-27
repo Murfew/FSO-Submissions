20660fb27e689fcbb51fc2cb9f4daee4ae2b2044
Murfew <matthew.hurley953@gmail.com>
2026-01-27 11:15:32 -0500

    Completed 9.20

--- part9/diaries/client/src/components/NewEntryForm.tsx
+++ part9/diaries/client/src/components/NewEntryForm.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react"
 import { createDiaryEntry } from "../services/diaryService"
-import type { NewEntryFormProps, Visibility, Weather } from "../types"
+import { Visibility, Weather, type NewEntryFormProps } from "../types"
 
 const NewEntryForm = (props: NewEntryFormProps) => {
   const [date, setDate] = useState('')
-  const [visibility, setVisibility] = useState('' as Visibility)
-  const [weather, setWeather] = useState('' as Weather)
+  const [visibility, setVisibility] = useState<Visibility>(Visibility.Great)
+  const [weather, setWeather] = useState<Weather>(Weather.Sunny)
   const [comment, setComment] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
 
@@ -14,8 +14,8 @@ const NewEntryForm = (props: NewEntryFormProps) => {
 
     try {
       const newEntry = await createDiaryEntry({date, visibility, weather, comment})
-
       props.handleNewEntry(newEntry)
+      setErrorMessage('')
     } catch (error) {
       setErrorMessage(error instanceof Error ? error.message : 'Unexpected error')
     }
@@ -34,14 +34,40 @@ const NewEntryForm = (props: NewEntryFormProps) => {
       )}
       <form onSubmit={addEntry}>
         <span style={{display:'block'}}>
-          date <input type="text" value={date} onChange={(event) => setDate(event.target.value)}/>
+          date <input type="date" value={date} onChange={(event) => setDate(event.target.value)}/>
         </span>
-        <span style={{display:'block'}}>
-          visibility <input type="text" value={visibility} onChange={(event) => setVisibility(event.target.value as Visibility)}/>
-        </span>
-        <span style={{display:'block'}}>
-          weather <input type="text" value={weather} onChange={(event) => setWeather(event.target.value as Weather)}/>
-        </span>
+        <fieldset>
+          <legend>Visibility</legend>
+
+          {Object.values(Visibility).map((value) => (
+            <label key={value} style={{ display: "block" }}>
+              <input
+                type="radio"
+                name="visibility"
+                value={value}
+                checked={visibility === value}
+                onChange={() => setVisibility(value)}
+              />
+              {value}
+            </label>
+          ))}
+        </fieldset>
+        <fieldset>
+          <legend>Weather</legend>
+
+          {Object.values(Weather).map((value) => (
+            <label key={value} style={{ display: "block" }}>
+              <input
+                type="radio"
+                name="weather"
+                value={value}
+                checked={weather === value}
+                onChange={() => setWeather(value)}
+              />
+              {value}
+            </label>
+          ))}
+        </fieldset>
         <span style={{display:'block'}}>
           comment <input type="text" value={comment} onChange={(event) => setComment(event.target.value)}/>
         </span>
