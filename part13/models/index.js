bdf8a8a95f007cf7e8ed9390789acfbbead28447
Murfew <matthew.hurley953@gmail.com>
2026-02-21 23:12:50 -0500

    Completed 13.19

--- part13/models/index.js
+++ part13/models/index.js
@@ -1,7 +1,11 @@
 import Blog from './blog.js'
+import ReadingList from './readingList.js'
 import User from './user.js'
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
-export { Blog, User }
+User.belongsToMany(Blog, { through: ReadingList })
+Blog.belongsToMany(User, { through: ReadingList })
+
+export { Blog, ReadingList, User }
---
+++ part13/models/readingList.js
@@ -0,0 +1,36 @@
+import { Model } from 'sequelize'
+import { sequelize } from '../util/db.js'
+
+class ReadingList extends Model {}
+
+ReadingList.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'id' },
+    },
+    blogId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'blogs', key: 'id' },
+    },
+    hasRead: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'readingList',
+  },
+)
+
+export default ReadingList
